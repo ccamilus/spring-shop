4900e942722ca99eec1ccbefbffd88617b8d880b
= <kobielarz.t@gmail.com>
2022-06-05 14:17:26 +0200

    EmailSenderService

--- src/main/java/pl/skefb/springshop/SpringShopApplication.java
+++ src/main/java/pl/skefb/springshop/SpringShopApplication.java
@@ -1,13 +1,26 @@
 package pl.skefb.springshop;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class SpringShopApplication {
+    @Autowired
+    private EmailSenderService senderService;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringShopApplication.class, args);
+    }
+
+    //Test EmailSender
+    @EventListener(ApplicationReadyEvent.class)
+    public void sendEmail() {
+        senderService.sendEmail("kobielarz.t@gmail.com",
+                "Test subject",
+                "Test body");
     }
 
 }
